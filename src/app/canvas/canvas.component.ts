0a006a631c2a29a9aa9d6c8019c270dab6216456
KoustuvP <kmukherjee564@gmail.com>
2019-11-14 14:39:44 +0530

    1 file modified

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { timeout } from 'rxjs/operators';
-//import { RequestNode } from './request-node'
+import { RequestNode } from './request-node'
 
 @Component({
   selector: 'app-canvas',
@@ -13,20 +13,21 @@ export class CanvasComponent implements OnInit {
   canvas: ElementRef<HTMLCanvasElement>;
 
   private ctx: CanvasRenderingContext2D;
-  //private node: RequestNode;
-
+  private node: RequestNode;
+  private id:number;
   constructor() { 
   
   }
 
   ngOnInit() {
-    //this.node=new RequestNode(1,20,75);
+    this.node=new RequestNode(1,20,75);
     this.ctx = this.canvas.nativeElement.getContext('2d');
-    // this.drawStructure();
-    // this.drawNode(this.node);
+    this.drawStructure();
+     this.drawNode(this.node);
+     this.animate();
   }
 
-  drawStructure():void{
+   drawStructure():void{
     this.ctx.beginPath();
     this.ctx.fillRect(10,50,300,3);
     this.ctx.fillRect(10,100,300,3);
@@ -37,12 +38,23 @@ export class CanvasComponent implements OnInit {
     this.ctx.stroke();
     this.ctx.closePath();
   }
-  // drawNode(node:RequestNode){
-  //   this.ctx.beginPath();
-  //   this.ctx.arc(node.xCord, node.yCord, 10, 0, 2 * Math.PI);
-  //   this.ctx.stroke();
-  //   this.ctx.closePath();
-  // }
+  drawNode(node:RequestNode){
+    this.ctx.beginPath();
+    this.ctx.arc(node.xCord, node.yCord, 10, 0, 2 * Math.PI);
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
+  animateArg(){
+      // drawNode(this.node);
+      // this.node.xCord+=10;
+      // this.ctx.clearRect(0,0,900,300);
+      // if(this.node.xCord>=450)
+      // clearInterval(this.id);
+      console.log(this.node,this.drawNode);
+  }
+  animate(){
+    this.id=setInterval(this.animateArg,1000)
+  }
 
 
  
